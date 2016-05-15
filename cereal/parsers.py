8659c9f07c669b15128e57d82f9045e7bb855a49
rightlag <rightlag@gmail.com>
2016-05-15 22:28:44 +0000

    Added syntax attribute to Protobuf class to differentiate between proto2 and proto3 syntax

--- cereal/parsers.py
+++ cereal/parsers.py
@@ -27,44 +27,56 @@ class Protobuf(ProtocolMeta):
 
     def __init__(self, filepath):
         super(Protobuf, self).__init__(filepath)
+        prog = re.compile(r'syntax\s=\s\"(proto\d+)\";')
+        with open(self._filepath) as fp:
+            match = prog.search(fp.read())
+        if match is not None:
+            syntax = match.group(1)
+        else:
+            syntax = 'proto2'
+        self._syntax = syntax
+
+    @property
+    def syntax(self):
+        return self._syntax
 
     def to_avro(self, indent=4):
         """Convert a Google Protocol Buffer file to an Apache Avro file."""
         schemas = []
         with open(self._filepath) as fp:
             lines = fp.readlines()
-            prog = re.compile(r'^message\s(\w+)\s\{$')
-            for i in range(len(lines)):
-                line = lines[i].strip()
-                match = prog.match(line)
-                if match is None:
+        prog = re.compile(r'^message\s(\w+)\s\{$')
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            match = prog.match(line)
+            if match is None:
+                continue
+            record = OrderedDict()
+            record['type'] = 'record'
+            # Google Protocol Buffer message name.
+            record['name'] = match.group(1)
+            record['fields'] = []
+            j = i
+            while True:
+                # Increment `j` by 1 to ignore the `message` line
+                # itself.
+                j += 1
+                line = lines[j].strip()
+                if line.endswith('}'):
+                    break
+                if line == '':
                     continue
-                record = OrderedDict()
-                record['type'] = 'record'
-                # Google Protocol Buffer message name.
-                record['name'] = match.group(1)
-                record['fields'] = []
-                j = i
-                while True:
-                    # Increment `j` by 1 to ignore the `message` line
-                    # itself.
-                    j += 1
-                    line = lines[j].strip()
-                    if line.endswith('}'):
-                        break
-                    if line == '':
-                        continue
-                    field = line.split()
-                    t, identifier = field[:2]
-                    try:
-                        t = self.PRIMITIVES[t]
-                    except KeyError:
-                        continue
-                    record['fields'].append({
-                        'name': identifier,
-                        'type': t,
-                    })
-                schemas.append(record)
+                field = line.split()
+                t, identifier = field[:2]
+                try:
+                    t = self.PRIMITIVES[t]
+                except KeyError:
+                    continue
+                record['fields'].append({
+                    'name': identifier,
+                    'type': t,
+                })
+            schemas.append(record)
         return json.dumps(schemas, indent=indent)
 
 
@@ -88,17 +100,18 @@ class Avro(ProtocolMeta):
         lines = ''
         with open(self._filepath) as fp:
             records = json.loads(fp.read())
-            for i in range(len(records)):
-                if i != 0:
-                    lines += '\n'
-                lines += 'message {}'.format(records[i]['name'])
-                lines += ' {\n'
-                fields = records[i]['fields']
-                for i in range(len(fields)):
-                    lines += ' ' * indent + '{} {} = {};\n'.format(
-                        self.PRIMITIVES[fields[i]['type']],
-                        fields[i]['name'],
-                        i + 1,
-                    )
-                lines += '}\n'
+        first = 0
+        last = len(records) - 1
+        for i in range(len(records)):
+            lines += '\n' if i != first else ''
+            lines += 'message {}'.format(records[i]['name'])
+            lines += ' {\n'
+            fields = records[i]['fields']
+            for j in range(len(fields)):
+                lines += ' ' * indent + '{} {} = {};\n'.format(
+                    self.PRIMITIVES[fields[j]['type']],
+                    fields[j]['name'],
+                    j + 1,
+                )
+            lines += '}' if i == last else '}\n'
         return lines
