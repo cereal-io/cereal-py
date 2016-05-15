7aa2a19de5097a6e013027b11260b47c47167b95
Brandon Him <brh55@drexel.edu>
2016-05-15 13:54:47 -0400

    Initial unit test

---
+++ tests/test_parser.py
@@ -0,0 +1,22 @@
+"""Tests for parser.py"""
+
+import unittest
+import json
+
+from cereal import build
+
+class ProtobufTestCase(unittest.TestCase):
+    def setUp(self):
+        self.svc = build('./examples/helloworld.proto')
+
+    def test_to_avro(self):
+        with open('./examples/helloworld.avsc') as fp:
+            expected = json.loads(fp.read())
+        actual = json.loads(self.svc.to_avro())
+        self.assertEqual(actual, expected)
+
+class AvroTestCase(unittest.TestCase):
+    pass
+
+class ThriftTestCase(unittest.TestCase):
+    pass
